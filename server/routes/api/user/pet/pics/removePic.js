6d12d6e298dc2e1a596cd6e4c9a9c0f088059237
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2019-09-28 17:28:04 -0300

    se quitó una respuesta mal dada

--- server/routes/api/user/pet/pics/removePic.js
+++ server/routes/api/user/pet/pics/removePic.js
@@ -1,27 +1,30 @@
-const Users = require('mongoose').model('Users')
+const Users = require("mongoose").model("Users")
 
-module.exports = function (req, res, next) {
-    const id = req.userId;
-    const id_pet = req.params.id_pet;
-    const id_pic = req.params.id_pic;
+module.exports = function(req, res, next) {
+    const id = req.userId
+    const id_pet = req.params.id_pet
+    const id_pic = req.params.id_pic
 
-    Users.findById(id).then(user => {
-        if (!user) {
-            res.sendStatus(400)
-        }
-        pet = user.pets.id(id_pet)
-        try {
-            pet.removePic(id_pic)
-        } catch (error) {
-            error.msj = "no se pudo remover"
-            throw error
-        }
-        user.save()
-        return user
-    }).then(r => {
-        res.status(200).send(r);
-    }).catch(e => {
-        e.status = 400
-        next(e);
-    })
+    Users.findById(id)
+        .then(user => {
+            if (!user) {
+                res.sendStatus(400)
+            }
+            pet = user.pets.id(id_pet)
+            try {
+                pet.removePic(id_pic)
+            } catch (error) {
+                error.msj = "no se pudo remover"
+                throw error
+            }
+            user.save()
+            return "Ok"
+        })
+        .then(r => {
+            res.status(200).send(r)
+        })
+        .catch(e => {
+            e.status = 400
+            next(e)
+        })
 }
