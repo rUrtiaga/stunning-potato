71afdd5aad939fba4361192c705f0e8cc81dd6dd
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2019-10-01 12:25:42 -0300

    agregado de numero random en nombre de la imagen
    se agregó un numero random en el nombre de las imagenes por conflicto
    cuando se subian multiples imagenes

--- server/routes/api/user/index.js
+++ server/routes/api/user/index.js
@@ -1,62 +1,76 @@
-const userRouter = require('express').Router()
-const auth = require('../../auth')
-var multer = require('multer')
+const userRouter = require("express").Router()
+const auth = require("../../auth")
+var multer = require("multer")
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function(req, file, cb) {
         dir = `./uploads/${req.userId}`
         if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
+            fs.mkdirSync(dir)
         }
         dir = `${dir}/${req.params.id_pet}`
         if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
+            fs.mkdirSync(dir)
         }
         cb(null, dir)
     },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + Date.now())
+    filename: function(req, file, cb) {
+        cb(
+            null,
+            file.fieldname +
+                Date.now() +
+                " - " +
+                Math.ceil(Math.random() * 1000)
+        )
     }
 })
 var upload = multer({
     storage: storage
 })
 
-const deletePets = require("./pet/deletePets");
+const deletePets = require("./pet/deletePets")
 
 userRouter.all("/:id/*", auth.required, auth.checkIdentity)
 
 //Pets
-userRouter.route("/:id/pets")
+userRouter
+    .route("/:id/pets")
     .post(require("./pet/newPet"))
     .get(require("./pet/obtainPets"))
     .delete(deletePets)
 
-userRouter.route("/:id/pets/:id_pet")
+userRouter
+    .route("/:id/pets/:id_pet")
     .get(require("./pet/obtainPet"))
     .delete(deletePets)
 
 //PICS
-userRouter.route("/:id/pets/:id_pet/pics")
+userRouter
+    .route("/:id/pets/:id_pet/pics")
     .post(
-        upload.fields([{
-            name: 'principal',
-            maxCount: 1
-        }, {
-            name: 'pics',
-            maxCount: 4
-        }]), require("./pet/pics/uploadPics"))
+        upload.fields([
+            {
+                name: "principal",
+                maxCount: 1
+            },
+            {
+                name: "pics",
+                maxCount: 4
+            }
+        ]),
+        require("./pet/pics/uploadPics")
+    )
     .get(require("./pet/pics/obtainPics"))
 
-userRouter.route("/:id/pets/:id_pet/pics/:id_pic")
+userRouter
+    .route("/:id/pets/:id_pet/pics/:id_pic")
     .delete(require("./pet/pics/removePic"))
     .get(require("./pet/pics/obtainPic"))
 
-userRouter.route("/:id/pets/:id_pet/search")
+userRouter
+    .route("/:id/pets/:id_pet/search")
     .post(require("./pet/search/newSearch"))
     .get(require("./pet/search/obtainSearch"))
     .delete(require("./pet/search/removeSearch"))
-
-
 
 module.exports = userRouter
