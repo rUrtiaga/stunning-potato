cb22f6b3333fe35f1028a1e6a1fd66f6b9be693c
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2019-11-19 18:00:47 -0300

    inicio de docker
    se hizo las configuraciones docker-compose para iniciar en develop y
    en deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    API_BASE_URL: "http://localhost:8000/api/",
-    here: {
-        id: "Zo2UBTaa0AJls5BZPgKT",
-        code: "-uxzhXs_Dzn3CJl8aq7GUw"
-    }
+  API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8000/api/",
+  here: {
+    id: "Zo2UBTaa0AJls5BZPgKT",
+    code: "-uxzhXs_Dzn3CJl8aq7GUw"
+  }
 }
